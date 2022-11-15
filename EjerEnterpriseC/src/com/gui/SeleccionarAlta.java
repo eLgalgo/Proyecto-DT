64e31d7dbadf331117193ab42f889588ac6e0a26
Lucas M <sergio.mansilla@estudiantes.utec.edu.uy>
2022-11-15 02:05:54 -0300

    Update SeleccionarAlta.java

--- EjerEnterpriseC/src/com/gui/SeleccionarAlta.java
+++ EjerEnterpriseC/src/com/gui/SeleccionarAlta.java
@@ -1,18 +1,14 @@
 package com.gui;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.naming.NamingException;
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SeleccionarAlta {
 
